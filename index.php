b08ef6c15a5f614e55112d87159d4438d50f8167
181710027 <181710027@LAB30810.senacinformatica.br>
2018-12-10 11:24:47 -0200

    yan

--- index.php
+++ index.php
@@ -16,6 +16,54 @@
         
         <?php
             
+            $lista = ReservaDAO::getReservas();
+            
+            if( count($lista) == 0 ){
+                echo '<h3>Não existem Reservas!</h3>';
+            } else {
+                
+                echo '<table border="1">';
+                echo '   <tr> ';
+                echo '      <th>Data Inicial</th> ';
+                echo '      <th>Data Final</th> ';
+                echo '      <th>Nome</th> ';
+                echo '      <th>Sala</th> ';
+                echo '   </tr> ';
+                
+                
+                
+                foreach ($lista as $reserva) {
+                    echo '<tr>';
+                    
+                    echo '  <td>'.$reserva->getDataInicial().'</td>';
+                    echo '  <td>'.$reserva->getDataFinal().'</td>';
+                    echo '  <td>'.$reserva->getUsuario()->getNome().'</td>';
+                    echo '  <td>'.$reserva->getSala().'</td>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+                
+                 echo '<table border="1">';
+                echo '   <tr> ';
+                echo '      <th>Data Inicial</th> ';
+                echo '      <th>Data Final</th> ';
+                echo '      <th>Nome</th> ';
+                echo '      <th>Material</th> ';
+                echo '   </tr> ';
+                
+                foreach ($lista as $reserva) {
+                    echo '<tr>';
+                    
+                    echo '  <td>'.$reserva->getHorarioInicial().'</td>';
+                    echo '  <td>'.$reserva->getHorarioFinal().'</td>';
+                    echo '  <td>'.$reserva->getUsuario()->getNome().'</td>';
+                    echo '  <td>'.$reserva->getMaterial().'</td>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+                
+            }
+        
         ?>
     </body>
 </html>
--- listarReservas.php
+++ listarReservas.php
@@ -1,17 +1,104 @@
+<?php
+    /*session_start();
+    if( isset($_SESSION['logado']) && $_SESSION['logado'] ){
+        
+        include_once 'model/clsCliente.php';
+        include_once 'model/clsPedido.php';
+        include_once 'dao/clsPedidoDAO.php';
+        include_once 'dao/clsConexao.php';
+        
+        $idCliente = 0;
+        
+        if( !isset( $_SESSION['admin'] ) || !$_SESSION['admin'] ){
+            $idCliente = $_SESSION['idCliente'];
+        }
+     
+     */
+?>
+     
+     
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Market M171 - Pedidos</title>
     </head>
     <body>
         <?php
-        // put your code here
+            require_once 'menu.php';
         ?>
+        <h1 align="center">Pedidos</h1>
+        <br><br><br>
+        
+        <?php
+            
+            $lista = ReservaDAO::getReservas();
+            
+            if( count($lista) == 0 ){
+                echo '<h3>Não existem Reservas!</h3>';
+            } else {
+                
+                echo '<table border="1">';
+                echo '   <tr> ';
+                echo '      <th>Data Inicial</th> ';
+                echo '      <th>Data Final</th> ';
+                echo '      <th>Nome</th> ';
+                echo '      <th>Sala</th> ';
+                echo '   </tr> ';
+                
+                
+                
+                foreach ($lista as $sala) {
+                    echo '<tr>';
+                    
+                    echo '  <td>'.$reserva->getDataInicial().'</td>';
+                    echo '  <td>'.$reserva->getDataFinal().'</td>';
+                    echo '  <td>'.$reserva->getUsuario()->getNome().'</td>';
+                    echo '  <td>'.$reserva->getSala().'</td>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+                
+                 echo '<table border="1">';
+                echo '   <tr> ';
+                echo '      <th>Data Inicial</th> ';
+                echo '      <th>Data Final</th> ';
+                echo '      <th>Nome</th> ';
+                echo '      <th>Material</th> ';
+                echo '   </tr> ';
+                
+                foreach ($lista as $material) {
+                    echo '<tr>';
+                    
+                    echo '  <td>'.$reserva->getHorarioInicial().'</td>';
+                    echo '  <td>'.$reserva->getHorarioFinal().'</td>';
+                    echo '  <td>'.$reserva->getUsuario()->getNome().'</td>';
+                    echo '  <td>'.$reserva->getMaterial().'</td>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+                
+            }
+        
+        ?>
+        
     </body>
 </html>
+
+<?php
+    
+        header("Location: index.php");
+    
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+

50ae3b11646b167ad253743d94b7fb56bd898501
Yan Matias Braga <181710027@senacinformatica.br>
2019-04-15 10:13:48 -0300

    Correções de merge na classe Sala

--- dao/clsMaterialDAO.php
+++ dao/clsMaterialDAO.php
@@ -1,66 +1,4 @@
-<<<<<<< HEAD
 <?php
-
-
-class MaterialDAO {
-    
-    public static function inserir( $material ){
-        $sql = "INSERT INTO materiais ( nome, qtdEstoque ) VALUES "
-                . " ( '".$material->getNome()."' , "
-                . "  '".$material->getQtdEstoque()."' ); ";
-        Conexao::executar($sql);
-        
-    }
-    
-    public static function editar( $material ){
-        $sql =    "UPDATE materiais SET "
-                . " nome = '".$material->getNome()."' , "
-                . " qtdEstoque = '".$material->getQtdEstoque()."'  "
-                . " WHERE codigo = ".$material->getCodigo();
-        Conexao::executar($sql);
-        
-    }
-    public static function excluir( $idMaterial ){
-        $sql =    "DELETE FROM materiais "
-                . " WHERE codigo = ".$idMaterial;
-        Conexao::executar($sql);
-        
-    }
-    
-    public static function getMateriais(){
-        $sql = "SELECT codigo, nome, qtdEstoque FROM materiais ORDER BY nome";
-        $result = Conexao::consultar($sql);
-        $lista = new ArrayObject();
-        if( $result != NULL ){
-            while( list($_cod, $_nome, $_qtdEstoque) = mysqli_fetch_row($result) ){
-                $material = new Material();
-                $material->setId($_cod);
-                $material->setNome($_nome);
-                $material->setqtdEstoque($_qtdEstoque);
-                $lista->append($material);
-            }
-        }
-        return $lista;
-    }
-    
-    
-    
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-=======
-<?php
-
 
 class MaterialDAO {
     
@@ -104,9 +42,8 @@ class MaterialDAO {
     }
     
     
-    
-    
 }
+    
 
 
 
@@ -118,4 +55,5 @@ class MaterialDAO {
 
 
 
->>>>>>> origin/master
+
+//>>>>>>> origin/master
--- model/clsSala.php
+++ model/clsSala.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-class Sala{
-    private $codigo;
-    private $numero, $descricao;
-    
-    function __construct($codigo = NULL, $numero = NULL, $descricao = NULL) {
-        $this->codigo = $codigo;
-        $this->numero = $numero;
-        $this->descricao = $descricao;
-    }
-    
-    function getCodigo() {
-        return $this->codigo;
-    }
-
-    function getNumero() {
-        return $this->numero;
-    }
-
-    function setCodigo($codigo) {
-        $this->codigo = $codigo;
-    }
-
-    function setNumero($numero) {
-        $this->numero = $numero;
-    }
-    function getDescricao() {
-        return $this->descricao;
-    }
-
-    function setDescricao($descricao) {
-        $this->descricao = $descricao;
-    }
-    
-=======
 <?php
 
 class Sala{
@@ -70,6 +33,5 @@ class Sala{
     function setDescricao($descricao) {
         $this->descricao = $descricao;
     }
-    
->>>>>>> origin/master
+
 }

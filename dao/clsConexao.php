48f9ddf6e84ee1b6f5d2eaeb51ace1d8cc7de956
analuizamattos <analu270397@gmail.com>
2021-05-16 00:06:04 -0300

    atualizacao

--- dao/clsConexao.php
+++ dao/clsConexao.php
@@ -8,11 +8,26 @@
  */
 class Conexao {
 
+//    private static function abrir() {
+//        $banco = "m171_04_projeto_integrador_prime";
+//        $local = "localhost";
+//        $usuario = "root";
+//        $senha = "";
+//
+//        $conn = mysqli_connect($local, $usuario, $senha, $banco);
+//
+//        if ($conn) {
+//            return $conn;
+//        } else {
+//            return NULL;
+//        }
+//    }
+
     private static function abrir() {
-        $banco = "m171_04_projeto_integrador_prime";
-        $local = "localhost";
-        $usuario = "root";
-        $senha = "";
+        $banco = "id16819099_projetoalugaja";
+        $local = "000webhost";
+        $usuario = "id16819099_anaalugaja";
+        $senha = "A246890zH6q8n0c$";
 
         $conn = mysqli_connect($local, $usuario, $senha, $banco);
 
@@ -57,4 +72,10 @@ class Conexao {
         }
     }
 
+//    public static function heroku($db) {
+//
+//        $db = parse_url(getenv("DATABASE_URL"));
+//        $db["path"] = ltrim($db["path"], "/");
+//        $conn = pg_connect(getenv("DATABASE_URL"));
+//    }
 }
--- dao/clsUsuarioDAO.php
+++ dao/clsUsuarioDAO.php
@@ -1,52 +1,50 @@
 <?php
- ///ESPERAR GRUPO PARA PODER CADASTRAR O USUARIO
 
 class UsuarioDAO {
-    
-    public static function inserir($usuario){
+
+    public static function inserir($usuario) {
         $sql = "INSERT INTO usuarios "
                 . " ( nomeCompleto, nomeUsuario, email, senha, "
                 . "   admin ) VALUES "
-                . " ( '".$usuario->getNomeCompleto()."' , "
-                . "   '".$usuario->getNomeUsuario()."' , "
-                . "   '".$usuario->getEmail()."' , "
-                . "   '".$usuario->getSenha()."' , "
-                . "    ".$usuario->getAdmin()
+                . " ( '" . $usuario->getNomeCompleto() . "' , "
+                . "   '" . $usuario->getNomeUsuario() . "' , "
+                . "   '" . $usuario->getEmail() . "' , "
+                . "   '" . $usuario->getSenha() . "' , "
+                . "    " . $usuario->getAdmin()
                 . "  ); ";
-        
-        Conexao::executar( $sql );
+
+        Conexao::executar($sql);
     }
-    
-    public static function editar($usuario){
-        $sql = "UPDATE usuarios SET " 
-                . " nomeCompleto =     '".$usuario->getNomeCompleto()."' , "
-                . " nomeUsuario = '".$usuario->getNomeUsuario()."' , "
-                . " email =      '".$usuario->getEmail()."' , "
-                . " senha =    '".$usuario->getSenha()."' , "
-                . " admin =    ".$usuario->getAdmin()."  "
-                . " WHERE codigo = ".$usuario->getId();
-        
-        Conexao::executar( $sql );
+
+    public static function editar($usuario) {
+        $sql = "UPDATE usuarios SET "
+                . " nomeCompleto =     '" . $usuario->getNomeCompleto() . "' , "
+                . " nomeUsuario = '" . $usuario->getNomeUsuario() . "' , "
+                . " email =      '" . $usuario->getEmail() . "' , "
+                . " senha =    '" . $usuario->getSenha() . "' , "
+                . " admin =    " . $usuario->getAdmin() . "  "
+                . " WHERE codigo = " . $usuario->getId();
+
+        Conexao::executar($sql);
     }
-    
-    
-    public static function excluir($usuario){
+
+    public static function excluir($usuario) {
         $sql = "DELETE FROM usuarios "
-             . " WHERE codigo =  ".$usuario->getId();
-        
-        Conexao::executar( $sql );
+                . " WHERE codigo =  " . $usuario->getId();
+
+        Conexao::executar($sql);
     }
-    
-    public static function getUsuarios(){
+
+    public static function getUsuarios() {
         $sql = " SELECT codigo, nomeCompleto, nomeUsuario, email,"
-             . " senha , admin "
-             . " FROM usuarios  "
-             . " ORDER BY nomeCompleto ";
-        
+                . " senha , admin "
+                . " FROM usuarios  "
+                . " ORDER BY nomeCompleto ";
+
         $result = Conexao::consultar($sql);
         $lista = new ArrayObject();
-        while( list( $cod, $nomeCompleto, $nomeUsuario, $email, $senha, $admin) = mysqli_fetch_row($result) ){
-            
+        while (list( $cod, $nomeCompleto, $nomeUsuario, $email, $senha, $admin) = mysqli_fetch_row($result)) {
+
             $usuario = new Usuario();
             $usuario->setId($cod);
             $usuario->setNomeCompleto($nomeCompleto);
@@ -54,54 +52,53 @@ class UsuarioDAO {
             $usuario->setEmail($email);
             $usuario->setSenha($senha);
             $usuario->setAdmin($admin);
-  
+
             $lista->append($usuario);
         }
-        
+
         return $lista;
     }
-    
-    
-   public static function getUsuarioById( $codigo ){
+
+    public static function getUsuarioById($codigo) {
         $sql = " SELECT codigo, nomeCompleto, nomeUsuario, "
-             . " email, senha, admin "
-             . " FROM usuarios "
-             . " WHERE codigo = ".$codigo;
-        
+                . " email, senha, admin "
+                . " FROM usuarios "
+                . " WHERE codigo = " . $codigo;
+
         $result = Conexao::consultar($sql);
-      
+
         list( $cod, $nomeCompleto, $nomeUsuario, $email, $senha, $admin) = mysqli_fetch_row($result);
-            $usuario = new Usuario();
-            $usuario->setId($cod);
-            $usuario->setNomeCompleto($nomeCompleto);
-            $usuario->setNomeUsuario($nomeUsuario);
-            $usuario->setEmail($email);
-            $usuario->setSenha($senha);
-            $usuario->setAdmin($admin);
-            
+        $usuario = new Usuario();
+        $usuario->setId($cod);
+        $usuario->setNomeCompleto($nomeCompleto);
+        $usuario->setNomeUsuario($nomeUsuario);
+        $usuario->setEmail($email);
+        $usuario->setSenha($senha);
+        $usuario->setAdmin($admin);
+
         return $usuario;
     }
-  
-    public static function logar($login, $senha){
+
+    public static function logar($login, $senha) {
         $sql = " SELECT codigo, nomeCompleto, nomeUsuario, email, senha, admin "
-             . " FROM usuarios "
-             . " WHERE ( nomeUsuario = '".$login."' OR email = '".$login."')  "
-             . "     AND senha = '".$senha."'    ";
+                . " FROM usuarios "
+                . " WHERE ( nomeUsuario = '" . $login . "' OR email = '" . $login . "')  "
+                . "     AND senha = '" . $senha . "'    ";
         $result = Conexao::consultar($sql);
-        
-        if( mysqli_num_rows( $result ) > 0 ){
-            $dados = mysqli_fetch_assoc( $result );
+
+        if (mysqli_num_rows($result) > 0) {
+            $dados = mysqli_fetch_assoc($result);
             $usuario = new Usuario();
-            $usuario->setId( $dados['codigo'] );
-            $usuario->setNomeCompleto( $dados['nomeCompleto'] );
-            $usuario->setNomeUsuario( $dados['nomeUsuario'] );
-            $usuario->setEmail( $dados['email'] );
-            $usuario->setSenha( $dados['senha'] );
-            $usuario->setAdmin( $dados['admin'] );
+            $usuario->setId($dados['codigo']);
+            $usuario->setNomeCompleto($dados['nomeCompleto']);
+            $usuario->setNomeUsuario($dados['nomeUsuario']);
+            $usuario->setEmail($dados['email']);
+            $usuario->setSenha($dados['senha']);
+            $usuario->setAdmin($dados['admin']);
             return $usuario;
         } else {
             return NULL;
         }
-        
-    }    
+    }
+
 }
